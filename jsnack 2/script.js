02d278134d8196ae4c99cd6d9f91295d1b952729
matteo-giustini <matteo.giustini3@gmail.com>
2024-12-03 12:42:12 +0100

    snack 2

---
+++ jsnack 2/script.js
@@ -0,0 +1,23 @@
+const invitati = ['Pippo', 'Franco', 'Ciccio', 'Matteo', 'Samuele']
+
+let guestname
+let isInvited = false
+
+do {
+    guestname = prompt("Inserisci il nome dell'invitato:")
+
+
+    for (let i = 0; i < invitati.length; i++) {
+        if (guestname === invitati[i]) {
+            isInvited = true;
+            break
+        }
+    }
+
+    if (isInvited) {
+        alert(`${guestname}, puoi entrare!`)
+    } else {
+        alert(`${guestname}, tu non puoi entrare!`)
+    }
+
+} while (!isInvited)
